2adf6dc5367187ef93ea2ad9f8343308afbfdcf0
AlcinoAfonso <alcinoafonso@unicodigital.com.br>
2025-05-27 22:42:37 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{fontFamily:'sans-serif',padding:'40px',textAlign:'center'}}>
+      <h1 style={{color:'#7A8AD6'}}>Quantum Vita – Landing Page</h1>
+      <p>Se você está vendo isto, o deploy funcionou ✔️</p>
+    </div>
+  );
+}
