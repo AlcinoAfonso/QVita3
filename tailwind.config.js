37477afd37c9464a824469a715a4b616b9eaef1f
AlcinoAfonso <alcinoafonso@unicodigital.com.br>
2025-05-28 11:53:14 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#7A8AD6",
+      },
+    },
+  },
+  plugins: [],
+};
